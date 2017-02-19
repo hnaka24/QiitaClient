a0ea453203c6d1588e9acbcb84934ee7e5cc97b9
中西博一 <hiroichi.nakanishi@nakanishihirokazu-no-iMac.local>
2017-02-19 21:01:19 +0900

    Initial commit

---
+++ app/src/main/kotlin/sample/qiitaclient/model/Article.kt
@@ -0,0 +1,9 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by hiroichi.nakanishi on 2017/02/18.
+ */
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
---
+++ app/src/main/kotlin/sample/qiitaclient/model/User.kt
@@ -0,0 +1,8 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by hiroichi.nakanishi on 2017/02/18.
+ */
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
