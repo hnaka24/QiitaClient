c88d7f949f20de68e563670628acd38192a4aa8e
hnaka24 <hiro1naka24m5@gmail.com>
2017-02-21 15:02:04 +0900

    第16章終了

--- app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
@@ -1,12 +1,16 @@
 package sample.qiitaclient
 
 import android.os.Bundle
+import android.view.View
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.ProgressBar
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle.kotlin.bindToLifecycle
 import rx.schedulers.Schedulers
 import rx.android.schedulers.AndroidSchedulers
 import retrofit2.*
@@ -15,21 +19,22 @@ import retrofit2.converter.gson.GsonConverterFactory
 import sample.qiitaclient.client.ArticleClient
 import sample.qiitaclient.model.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : RxAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listAdapter = ArticleListAdapter(applicationContext)
-        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "たろう"),
-                dummyArticle("Java入門", "じろう"))
-
         val listView = findViewById(R.id.list_view) as ListView
+        val progressBar = findViewById(R.id.progress_bar) as ProgressBar
+        val queryEditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton = findViewById(R.id.search_button) as Button
+
+        val listAdapter = ArticleListAdapter(applicationContext)
         listView.adapter = listAdapter
         listView.setOnItemClickListener { adapterView, view, position, id ->
-            val article = listAdapter.articles[position]
-            ArticleActivity.intent(this, article).let { startActivity(it) }
+            val intent = ArticleActivity.intent(this, listAdapter.articles[position])
+            startActivity(intent)
         }
 
         // APIクライアントの設定と生成
@@ -44,13 +49,16 @@ class MainActivity : AppCompatActivity() {
         val articleClient = retrofit.create(ArticleClient::class.java)
 
         // 検索ボタンのタップ時の処理
-        val queryEditText = findViewById(R.id.query_edit_text) as EditText
-        val searchButton = findViewById(R.id.search_button) as Button
-
         searchButton.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+
             articleClient.search(queryEditText.text.toString())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
+                    .doAfterTerminate {
+                        progressBar.visibility = View.GONE
+                    }
+                    .bindToLifecycle(this)
                     .subscribe({
                         queryEditText.text.clear()
                         listAdapter.articles = it
