5952231fcc05dc7730ec07c5c65cb73e0414f575
hnaka24 <hiro1naka24m5@gmail.com>
2017-02-21 22:34:19 +0900

    第18章の1まで終了

--- app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.ProgressBar
+import butterknife.bindView
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
 import com.trello.rxlifecycle.kotlin.bindToLifecycle
 import rx.schedulers.Schedulers
@@ -18,15 +19,18 @@ class MainActivity : RxAppCompatActivity() {
     @Inject
     lateinit var articleClient: ArticleClient
 
+    val listView: ListView by bindView(R.id.list_view)
+
+    val progressBar: ProgressBar by bindView(R.id.progress_bar)
+    
+    val searchButton: Button by bindView(R.id.search_button)
+
+    val queryEditText: EditText by bindView(R.id.query_edit_text)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (application as QiitaClientApp).component.inject(this)
         setContentView(R.layout.activity_main)
-
-        val listView = findViewById(R.id.list_view) as ListView
-        val progressBar = findViewById(R.id.progress_bar) as ProgressBar
-        val queryEditText = findViewById(R.id.query_edit_text) as EditText
-        val searchButton = findViewById(R.id.search_button) as Button
 
         val listAdapter = ArticleListAdapter(applicationContext)
         listView.adapter = listAdapter
